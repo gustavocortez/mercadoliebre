f8f82259cb3893b9c1a5c980e44da7427f4f0a70
gustavocortez <crt.gustavo@gmail.com>
2023-11-07 15:09:01 -0300

    Cambios en la configuración

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 3001;
+
 //Indicar archivos estáticos
 app.use(express.static('public'));
 
 //Levantar el servidor
-app.listen(3001, () => {
-    console.log('Servidor corriendo en el puerto 3001');
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
 app.get('/', (req, res) =>{
